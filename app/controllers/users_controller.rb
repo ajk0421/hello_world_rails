bd8186b98ea1b541b94effa3dd9c869e8fc033f5
ajk0421 <ajk.spirituallounge@gmai.com>
2019-04-10 20:33:22 +0900

    Lv1.終了

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+  before_action :set_user, only:[:show,:update,:destroy]
+  def index
+    @users = User.all
+  end
+
+  def create
+    User.create!(user_params)
+  end
+
+  def show
+  end
+
+  def update
+    binding.pry
+    @user.update!(user_params)
+  end
+
+  def destroy
+    @user.destroy!
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:account, :name, :email)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
