3d213385bc14c8c8194536ee39e0dfb3e4003d2d
ajk0421 <ajk.spirituallounge@gmai.com>
2019-04-10 20:33:22 +0900

    Lv1.終了

--- config/application.rb
+++ config/application.rb
@@ -17,10 +17,13 @@ module HelloWorldRails
     # the framework and any gems in your application.
 
     config.generators do |g|
+      g.template_engine false
       g.javascripts false
       g.stylesheets false
       g.helper false
       g.test_framework false
     end
+    config.api_only = true
+      config.middleware.use ActionDispatch::Flash
   end
 end
