4e3efb44164093daf7ca8ee1c85b2f8f2d387546
ajk0421 <ajk.spirituallounge@gmai.com>
2019-04-13 18:52:56 +0900

    課題3 articles_table status_column null_false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_12_045213) do
+ActiveRecord::Schema.define(version: 2019_04_13_094937) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2019_04_12_045213) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    t.integer "status", default: 0
+    t.integer "status", default: 0, null: false
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
